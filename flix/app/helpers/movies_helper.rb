4a844c320c8c7688d4ad00f8ada0bcba906de047
Sam Marten <sammarten@gmail.com>
2014-11-30 16:36:33 -0600

    Helpers

--- flix/app/helpers/movies_helper.rb
+++ flix/app/helpers/movies_helper.rb
@@ -1,2 +1,7 @@
 module MoviesHelper
+  
+  def format_total_gross(movie)
+    movie.flop? ? content_tag(:strong, "Flop!") : number_to_currency(movie.total_gross)
+  end
+  
 end
--- flix/app/models/movie.rb
+++ flix/app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ActiveRecord::Base
+
+  def flop?
+    total_gross < 50000000
+  end
 end
