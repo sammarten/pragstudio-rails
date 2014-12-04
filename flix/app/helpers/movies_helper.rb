94be15bef46924e9bebafbc5a9d3378611f3df10
Sam Marten <sammarten@gmail.com>
2014-12-04 17:54:35 -0600

    Test-First: Setup

--- flix/app/helpers/movies_helper.rb
+++
@@ -1,7 +0,0 @@
-module MoviesHelper
-  
-  def format_total_gross(movie)
-    movie.flop? ? content_tag(:strong, "Flop!") : number_to_currency(movie.total_gross)
-  end
-  
-end
--- flix/app/models/movie.rb
+++
@@ -1,6 +0,0 @@
-class Movie < ActiveRecord::Base
-
-  def flop?
-    total_gross < 50000000
-  end
-end
